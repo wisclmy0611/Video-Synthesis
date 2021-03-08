05370659a5f66a5efa7989f3d2b9740d0955b69d
Mingyi <wisclmy0611@gmail.com>
2021-03-07 20:40:05 -0800

    add discriminator

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import torch
+import torch, utils
 from torch import nn
 import torch.nn.functional as F
 
@@ -107,4 +107,36 @@ class Generator(nn.Module):
 
         out = self.out_layer(out)
         return out
-        
+
+class discriminator(nn.Module):
+    # initializers
+    def __init__(self, nf=64):
+        super(discriminator, self).__init__()
+        self.nf = nf
+        self.convs = nn.Sequential(
+            nn.Conv2d(3, nf, 3, 1, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf, nf * 2, 3, 2, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 2, nf * 4, 3, 1, 1),
+            nn.InstanceNorm2d(nf * 4),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 4, nf * 4, 3, 2, 1),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 4, nf * 8, 3, 1, 1),
+            nn.InstanceNorm2d(nf * 8),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 8, nf * 8, 3, 1, 1),
+            nn.InstanceNorm2d(nf * 8),
+            nn.LeakyReLU(0.2, True),
+            nn.Conv2d(nf * 8, 1, 3, 1, 1),
+            nn.Sigmoid(),
+        )
+
+        utils.initialize_weights(self)
+
+    # forward method
+    def forward(self, input):
+        output = self.convs(input)
+
+        return output
---
+++ utils.py
@@ -0,0 +1,16 @@
+import torch.nn as nn
+
+def initialize_weights(net):
+    for m in net.modules():
+        if isinstance(m, nn.Conv2d):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.ConvTranspose2d):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.Linear):
+            m.weight.data.normal_(0, 0.02)
+            m.bias.data.zero_()
+        elif isinstance(m, nn.BatchNorm2d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
