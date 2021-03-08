775c4f9c0c9a748a297f9d1992a0942bd053fa33
wisclmy0611 <mingyilu@cs.stanford.edu>
2021-03-07 20:23:40 -0800

    Update video2anime script

--- video2anime.sh
+++ video2anime.sh
@@ -1,6 +1,7 @@
 #/bin/bash
 
 rm -f samples/inputs/*
+FPS=$(ffprobe input.mp4 2>&1 | grep fps | sed 's/^.*, \(.*\) fps.*$/\1/')
 ffmpeg -i input.mp4 -q:v 5 samples/inputs/img%03d.jpg
-python test.py --device cpu
-ffmpeg -i samples/results/img%03d.jpg -r 30 output.mp4
+python test.py
+ffmpeg -i samples/results/img%03d.jpg -r $FPS output.mp4
