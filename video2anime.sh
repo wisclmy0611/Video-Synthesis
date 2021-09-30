2acf41aadac57a1c60b5c616525a598b9c0d609c
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-30 22:54:56 +0800

    Delete video2anime.sh

--- video2anime.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-rm -f samples/inputs/*
-FPS=$(ffprobe input.mp4 2>&1 | grep fps | sed 's/^.*, \(.*\) fps.*$/\1/')
-ffmpeg -i input.mp4 -q:v 5 samples/inputs/img%03d.jpg
-python test.py
-ffmpeg -i samples/results/img%03d.jpg -r $FPS output.mp4
